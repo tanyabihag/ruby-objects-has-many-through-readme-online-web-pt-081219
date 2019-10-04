c171f6ec87b473c9963f6892abee8c817fd907ba
Tanya Bihag <tbihag@gmail.com>
2019-10-04 20:25:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,4 +17,10 @@ end
 def new_meal(customer, total, tip=0)
   Meal.new(self, customer, total, tip)
 end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
 end
