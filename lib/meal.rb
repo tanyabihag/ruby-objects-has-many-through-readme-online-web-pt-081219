088652f3d741a55f2f57fb474bba00ae459c1298
Tanya Bihag <tbihag@gmail.com>
2019-10-04 20:03:34 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,20 @@
 class Meal
 
+attr_accessor :waiter, :customer, :total, :tip
+@@all = []
 
-def initialize
+def initialize(waiter, customer, total, tip=0)
+  @waiter = waiter
+  @customer = customer
+  @total = total
+  @tip = tip
+  @@all << self
+  
+end
+
+def self.all
+  @@all
+end
+
+
 end
